ab9ac97e513812ddbede4a4a281ff7e8fc0c59b7
Colin T.A. Gray <colinta@gmail.com>
2018-05-15 09:29:00 -0600

    update View helper extensions

--- app/src/main/java/co/ello/android/ello/ext/ImageView.kt
+++ app/src/main/java/co/ello/android/ello/ext/ImageView.kt
@@ -1,13 +1,22 @@
 package co.ello.android.ello
 
 import android.widget.ImageView
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
+import java.lang.Exception
 import java.net.URL
 
 
-fun ImageView.setImageURL(url: URL?) {
-    loadImageURL(url).into(this)
+fun ImageView.setImageURL(url: URL?, onSuccess: Block? = null, onError: Block? = null) {
+    loadImageURL(url).into(this, object : Callback {
+        override fun onSuccess() {
+            onSuccess?.invoke()
+        }
+        override fun onError(e: Exception?) {
+            onError?.invoke()
+        }
+    })
 }
 
 fun loadImageURL(url: URL?): RequestCreator = Picasso.get().load(url?.toString())
--- app/src/main/java/co/ello/android/ello/ext/ViewGroup.kt
+++ app/src/main/java/co/ello/android/ello/ext/ViewGroup.kt
@@ -4,4 +4,4 @@ import android.view.View
 import android.view.ViewGroup
 
 
-val ViewGroup.childViews: Iterator<View> get() = (0 until childCount).map { getChildAt(it) }.iterator()
+val ViewGroup.childViews: List<View> get() = (0 until childCount).map { getChildAt(it) }
