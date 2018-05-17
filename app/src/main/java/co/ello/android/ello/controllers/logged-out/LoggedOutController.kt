534653a37d9027ea860692ab4d4b7603c2751fd7
Colin T.A. Gray <colinta@gmail.com>
2018-05-17 10:42:46 -0600

    remove artist invites and editorials from logged out (for now - see tag 'had-home-tabs' to undo)

--- app/src/main/java/co/ello/android/ello/controllers/logged-out/LoggedOutController.kt
+++ app/src/main/java/co/ello/android/ello/controllers/logged-out/LoggedOutController.kt
@@ -6,42 +6,26 @@ import android.view.View
 class LoggedOutController(
     a: AppActivity,
     val delegate: LoggedOutProtocols.Delegate
-    ) : BaseController(a), LoggedOutProtocols.Controller, LoginProtocols.Delegate, JoinProtocols.Delegate, HomeProtocols.Delegate
+    ) : BaseController(a), LoggedOutProtocols.Controller, LoginProtocols.Delegate, JoinProtocols.Delegate
 {
     private lateinit var screen: LoggedOutProtocols.Screen
 
-    private val editorialsController = EditorialsController(activity)
-    private val artistInvitesController = CategoryController(activity)
     private val discoverController = CategoryController(activity)
-    private val homeController: HomeController
 
-    init {
-        homeController = HomeController(activity, delegate = this,
-            childControllers = listOf(
-                Pair(T(R.string.Editorials_title), editorialsController),
-                Pair(T(R.string.ArtistInvites_title), artistInvitesController),
-                Pair(T(R.string.Discover_title), discoverController)
-                ),
-            selected = 2)
-     }
-
-    override val childControllers: Iterable<Controller> get() { return listOf(homeController) }
+    override val childControllers: Iterable<Controller> get() { return listOf(discoverController) }
     override val visibleChildControllers: Iterable<Controller> get() { return childControllers }
-    override val firstResponder: Controller get() = homeController.firstResponder
+    override val firstResponder: Controller get() = discoverController.firstResponder
 
     override fun createView(): View {
         val screen = LoggedOutScreen(activity)
-        screen.containerView.addView(homeController.view)
+        screen.containerView.addView(discoverController.view)
         screen.delegate = this
         this.screen = screen
         return screen.contentView
     }
 
-    override fun homeTabSelected(tab: Int) {
-    }
-
     override fun onStart() {
-        homeController.assignParent(this, isVisible = true)
+        discoverController.assignParent(this, isVisible = true)
     }
 
     override fun showJoinScreen() {
