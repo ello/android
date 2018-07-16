61fa293abe64f115b4054b3c1ed0bdd8cd05060c
Colin T.A. Gray <colinta@gmail.com>
2018-07-16 15:10:48 -0400

    assign user.location to location cell TextView

--- app/src/main/java/co/ello/android/ello/controllers/profile/cells/ProfileHeaderLocationCell.kt
+++ app/src/main/java/co/ello/android/ello/controllers/profile/cells/ProfileHeaderLocationCell.kt
@@ -2,7 +2,17 @@ package co.ello.android.ello
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class ProfileHeaderLocationCell(parent: ViewGroup) : StreamCell(LayoutInflater.from(parent.context).inflate(R.layout.profile_header_location_cell, parent, false)) {
+    val locationLabel: TextView = itemView.findViewById(R.id.locationLabel)
+
+    data class Config(
+        val location: String
+        )
+
+    fun config(value: Config) {
+        locationLabel.text = value.location
+    }
 }
--- app/src/main/java/co/ello/android/ello/controllers/profile/cells/ProfileHeaderLocationPresenter.kt
+++ app/src/main/java/co/ello/android/ello/controllers/profile/cells/ProfileHeaderLocationPresenter.kt
@@ -3,5 +3,11 @@ package co.ello.android.ello
 
 object ProfileHeaderLocationPresenter {
     fun configure(cell: ProfileHeaderLocationCell, item: StreamCellItem) {
+        val user = item.model as? User ?: return
+        val location = user.location ?: return
+
+        cell.config(ProfileHeaderLocationCell.Config(
+            location = location
+            ))
     }
 }
